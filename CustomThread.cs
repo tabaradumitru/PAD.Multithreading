6fa1f23abcb9742fc6edebe44332bbc71053b538
Dumitru Tabără <dumitru.tabara@codwer.com>
2021-01-17 23:35:32 +0200

    initial commit

---
+++ CustomThread.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Multithreading
+{
+    public class CustomThread
+    {
+        private readonly CountdownEvent _countdown;
+        private readonly List<CountdownEvent> _waiters;
+        private int _threadsToWait;
+        private readonly int _workTime;
+        private readonly Thread _thread;
+        private string Name { get; }
+
+        public CustomThread(string name, int workTime)
+        {
+            Name = name;
+            _workTime = workTime;
+            _countdown = new CountdownEvent(0);
+            _waiters = new List<CountdownEvent>();
+            _thread = new Thread(DoWork);
+        }
+
+        public void WaitFor(params CustomThread[] threadsToWait)
+        {
+            if (_thread.ThreadState == ThreadState.Running) throw new ThreadStateException("Thread is running");
+
+            foreach (var thread in threadsToWait)
+            {
+                if (thread == this || thread._waiters.Contains(_countdown)) continue;
+                thread._waiters.Add(_countdown);
+                _threadsToWait++;
+            }
+        }
+
+        private void DoWork()
+        {
+            try
+            {
+                _countdown.Reset(_threadsToWait);
+                _countdown.Wait();
+                Console.WriteLine(Name);
+                Thread.Sleep(_workTime);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                foreach (var waiter in _waiters) waiter.Signal();
+            }
+        }
+
+        public void Start() => _thread.Start();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Multithreading
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var t1 = new CustomThread("Thread 1", GetRandomNumber(3000));
+            var t2 = new CustomThread("Thread 2", GetRandomNumber(3000));
+            var t3 = new CustomThread("Thread 3", GetRandomNumber(3000));
+            var t4 = new CustomThread("Thread 4", GetRandomNumber(3000));
+            var t5 = new CustomThread("Thread 5", GetRandomNumber(3000));
+            var t6 = new CustomThread("Thread 6", GetRandomNumber(3000));
+            var t7 = new CustomThread("Thread 7", GetRandomNumber(3000));
+
+            t2.WaitFor(t1);
+            t3.WaitFor(t1, t2, t4, t5, t6, t7);
+            t4.WaitFor(t6);
+            t5.WaitFor(t2);
+            t7.WaitFor(t4);
+
+            CustomThread[] threads = { t1, t2, t3, t4, t5, t6, t7 };
+            
+            foreach (var thread in threads) thread.Start();
+        }
+
+        private static int GetRandomNumber(int max) => new Random().Next(max);
+    }
+}
